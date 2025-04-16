f63331e8d53ba08f57dccbc9ab898f7ca0208db5
Dimasrizky <dimasrizky822@gmail.com>
2025-04-16 08:55:56 +0700

    students dashboard

---
+++ frontend/src/app/dashboard/student/loading.tsx
@@ -0,0 +1,37 @@
+import { Skeleton } from '@/components/ui/skeleton'
+import { Card, CardContent, CardFooter } from '@/components/ui/card'
+
+export default function StudentLoading() {
+  return (
+    <div className="container flex h-screen w-screen flex-col items-center justify-center">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="flex flex-col space-y-2 text-center">
+          <Skeleton className="h-8 w-3/4 mx-auto" />
+          <Skeleton className="h-4 w-full mx-auto" />
+        </div>
+
+        <Card>
+          <CardContent className="pt-6">
+            <div className="space-y-4">
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-1/4" />
+                <Skeleton className="h-10 w-full" />
+              </div>
+              <div className="space-y-2">
+                <div className="flex items-center justify-between">
+                  <Skeleton className="h-4 w-1/4" />
+                  <Skeleton className="h-4 w-1/4" />
+                </div>
+                <Skeleton className="h-10 w-full" />
+              </div>
+              <Skeleton className="h-10 w-full" />
+            </div>
+          </CardContent>
+          <CardFooter className="flex justify-center">
+            <Skeleton className="h-4 w-2/3" />
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+  )
+}
